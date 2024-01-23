f516e96cd2b334a5600963f9e2628f77c0de0072
Adil <madil6583@gmail.com>
2024-01-23 20:34:13 +0600

    clean

--- cmd/main.go
+++ cmd/main.go
@@ -26,32 +26,3 @@ func main() {
 		logger.InfoLog.Println("Server running, listening on http://localhost:9000/")
 	}
 }
-
-// func test() {
-// 	_, err := strconv.Atoi("")
-// 	fmt.Println(err)
-// }
-
-// func testLinks() {
-// 	link := requests.Agify + "Adil"
-// 	println(link)
-
-// 	req, err := http.NewRequest(http.MethodGet, link, nil)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		return
-// 	}
-// 	resp, err := http.DefaultClient.Do(req)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		return
-// 	}
-// 	body, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		return
-// 	}
-// 	var answer structures.PersonAgify
-// 	json.Unmarshal(body, &answer)
-// 	fmt.Println(answer)
-// }
